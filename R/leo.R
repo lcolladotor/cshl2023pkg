9d630b5a93cf39cd3042acda730d958c8bc2a693
lcolladotor <lcolladotor@gmail.com>
2023-07-03 19:46:15 -0400

    First commit before making the documentation website

---
+++ R/leo.R
@@ -0,0 +1,17 @@
+#' Print the name Leo
+#'
+#' This function is a test function that returns the name
+#' Leo.
+#'
+#' @return A `character(1)` with the words `Leo`.
+#' @export
+#'
+#' @examples
+#' ## Print the name Leo
+#' leo()
+#'
+#' ## Capitalize the first letter
+#' gsub("l", "L", leo())
+leo <- function() {
+    "leo"
+}
---
+++ tests/testthat/test-leo.R
@@ -0,0 +1,3 @@
+test_that("leo works", {
+  expect_equal(leo(), "leo")
+})
